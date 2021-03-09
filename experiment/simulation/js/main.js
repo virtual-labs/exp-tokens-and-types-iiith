e87e4e9e797dbff7b5c0504d2f3b4c7bf7398543
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2021-03-09 19:00:30 +0530

    Updated simulation

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,1 +1,17 @@
-//Your JavaScript goes in here
+function selectCorpus()
+{
+var corp = document.getElementById("corp_opt");
+theIndx=corp.selectedIndex;
+corp_id=corp.options[theIndx].value;
+if(corp_id=="-1")
+	{
+		alert("Select a corpus");
+		return;
+	}
+
+id=corp_id.split("*");
+var cno=parseFloat(id[1]);
+var lno=parseFloat(id[0]);
+$('#opt').load('CLExperiment3-1.php?ind='+cno+'&lang='+lno);
+
+}
