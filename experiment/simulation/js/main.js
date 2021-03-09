57ca49c067755f16b88b521ef088f2fded530c3e
mrudhvika940 <mrudhvikadamaraju@gmail.com>
2021-03-09 19:15:24 +0530

    Updated main.js

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -1,17 +1,56 @@
-function selectCorpus()
+
+function selectLang()
 {
-var corp = document.getElementById("corp_opt");
-theIndx=corp.selectedIndex;
-corp_id=corp.options[theIndx].value;
-if(corp_id=="-1")
+
+var lang = document.getElementById("lang_opt");
+theIndx=lang.selectedIndex;
+lang_id=lang.options[theIndx].value;
+if(lang_id=="0")
 	{
-		alert("Select a corpus");
+		alert("Select Language");
 		return;
 	}
+var lno=parseFloat(lang_id);
+$('#corpus_opt').load('CLExperiment4-0.php?ind='+lno);
+}
 
-id=corp_id.split("*");
+
+function selectSize()
+{
+var tokens = document.getElementById("corp_size");
+theIndx=tokens.selectedIndex;
+tok_id=tokens.options[theIndx].value;
+id=tok_id.split("*");
+
+if(tok_id=="-1")
+	{
+		alert("Select the number of tokens");
+		return;
+	}
+var tno=parseFloat(id[1]);
+var lno=parseFloat(id[0]);
+$('#display').load('CLExperiment4-2.php?ind='+tno+'&lang='+lno);
+
+}
+
+
+function selectSize()
+{
+var tokens = document.getElementById("corp_size");
+theIndx=tokens.selectedIndex;
+tok_id=tokens.options[theIndx].value;
+alert(tok_id);
+id=tok_id.split("*");
+
+if(tok_id=="-1")
+	{
+		alert("Select the number of tokens");
+		return;
+	}
+var tno=parseFloat(id[2]);
 var cno=parseFloat(id[1]);
 var lno=parseFloat(id[0]);
-$('#opt').load('CLExperiment3-1.php?ind='+cno+'&lang='+lno);
+
+$('#display').load('CLExperiment4-2.php?ind='+tno+'&corp='+cno+'&lang='+lno);
 
 }
