078cb8d93ca9ab3f34a279df4bf315074de7f38d
balammaboya <balamma38@gmail.com>
2021-09-16 20:27:19 +0530

    added supportive files

---
+++ experiment/simulation/CLintroduction3.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<script class='gtm'>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-W59SWTR');</script>
+
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Untitled Document</title>
+</head>
+
+<body>
+<div style="font-size:15px;" >
+<center>
+Tokens correspond to the total number of word counts in a text while type corresponds to the total count of unique words in a text. <br/><br/>
+
+<img src="Exp3/Exp3.jpg" alt="ALT_tokens-typesfig" height="15%" width="25%" /> </center>
+</div>
+</body>
+</html>
